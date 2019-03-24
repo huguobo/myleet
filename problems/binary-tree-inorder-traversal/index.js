44d7fff58aaa5470e507004b18909331df10a869
huguobo <huguobo@meituan.com>
2019-03-24 22:49:28 +0800

    feat: problem binary-tree-inorder-travel use iteration method

--- problems/binary-tree-inorder-traversal/index.js
+++ problems/binary-tree-inorder-traversal/index.js
@@ -1,13 +1,32 @@
 /* eslint-disable no-unused-expressions */
 //  中序遍历树
-var inorderTraversal = function (root, res = []) {
-  if (!root) {
-    return res;
+
+// 迭代 Iteration
+var inorderTraversal = function (root) {
+  var stack = [];
+  var res = [];
+  while (root || stack.length) {
+    if (root) {
+      stack.push(root);
+      root = root.left;
+    } else {
+      root = stack.pop();
+      res.push(root.val);
+      root = root.right;
+    }
   }
-  root.left && inorderTraversal(root.left, res);
-  res.push(root.val);
-  root.right && inorderTraversal(root.right, res);
   return res;
 };
+
+// 递归 Recursion
+// var inorderTraversal = function (root, res = []) {
+//   if (!root) {
+//     return res;
+//   }
+//   root.left && inorderTraversal(root.left, res);
+//   res.push(root.val);
+//   root.right && inorderTraversal(root.right, res);
+//   return res;
+// };
 
 module.exports = inorderTraversal;
