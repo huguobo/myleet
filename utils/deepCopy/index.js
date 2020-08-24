26d03e30700cbb999cabd0600ecbce7c9645405a
“huguobo” <huguobo2010@126.com>
2020-08-24 17:12:18 +0800

    feat(): debounce

--- utils/deepCopy/index.js
+++ utils/deepCopy/index.js
@@ -1,7 +1,7 @@
 // 使用递归的方式实现数组、对象的深拷贝
 function deepClone(obj) {
   // 判断拷贝的要进行深拷贝的是数组还是对象，是数组的话进行数组拷贝，对象的话进行对象拷贝
-  var objClone = Array.isArray(obj) ? [] : {};
+  let objClone = Array.isArray(obj) ? [] : {};
   // 进行深拷贝的不能为空，并且是对象或者是
   if (obj && typeof obj === 'object') {
     for (let key in obj) {
