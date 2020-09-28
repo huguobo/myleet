020319b0ea5644d0b17dd7f3fee92d07b0bdca52
guobo.hu <guobo.hu@hotstar.com>
2020-09-28 13:16:47 +0800

    doc(): add some doc

--- problems/populating-next-right-pointers-in-each-node-ii/index.js
+++ problems/populating-next-right-pointers-in-each-node-ii/index.js
@@ -39,6 +39,8 @@ const connect = root => {
     root.right.next = findNext(root.next);
   }
   // 先 right
+  // 如果我们先left再right的话，我们的9是无法指向10的。因为我们的5.right是空，所以我们会通过findNext(5.next)去寻找非空节点，但是由于6的左右都为空，所以会继续findNext(6.next)，但是此时我们6和7还未链接，所以结果就是8 → 9 → null。
+  // 但是如果我们先right再left的话，此时我们的6和7可以保证链接，那么此时findNext(6)找不到的话，我们会继续findNext(6.next)也就是findNext(7)。
   connect(root.right);
   connect(root.left);
   return root;
